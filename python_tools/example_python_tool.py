ef6f56e003e212428e103077ed9efe8444f47fde
Ian <ionox0@gmail.com>
2018-05-04 12:54:40 -0400

    Update example_python_tool.py

--- python_tools/example_python_tool.py
+++ python_tools/example_python_tool.py
@@ -1,20 +1,22 @@
 import argparse
 
 
-def make_fingerprint(output_filename):
+def make_a_file(output_filename):
     print "This will be printed to stdout and the log"
 
     with open(output_filename, 'w') as f:
-        f.write('HELLO FINGERPRINT')
+        f.write('HELLO EXAMPLE OUTPUT')
 
 
-def do_fingerprint(args):
-    make_fingerprint(args.output_file)
+def do_stuff(args):
+    make_a_file(args.output_file)
 
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--input_file", help="picard interval list", required=True)
+    # Sometimes we want the names of the output files to be provided as inputs
+    # That makes it easer to reference them from the CWL files:
     # parser.add_argument("-o", "--output_file", help="output bed file", required=True)
     args = parser.parse_args()
     return args
